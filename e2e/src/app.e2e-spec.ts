e8809680e14dbab397531a3d14db10da967b2706
Bart Verhoeven <bart.verhoeven144@gmail.com>
2019-03-03 13:08:00 +0100

    syntax error in testfile

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(true
+    expect(true);
   });
 
   afterEach(async () => {
